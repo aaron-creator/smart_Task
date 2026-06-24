6aac930b5bc0ed71852a8ca38f92cc6a193bdcf9
aaron-creator <aaronpsam@gmail.com>
2026-06-24 16:50:35 +0530

    Initial commit

---
+++ taskmanagement-backend/src/main/java/com/taskmanagement/model/User.java
@@ -0,0 +1,38 @@
+package com.taskmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean active;
+
+    private LocalDateTime createdAt;
+
+}
+
